e589c284fc636d05c049d9027ce51823f6a102fc
GH-Marc <marcelopgm65@gmail.com>
2022-06-23 23:59:02 -0300

    Day 4 done! :tada:

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4ofern801zd01z4g7t216jf/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
